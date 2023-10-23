f3391baac3cb7b181c4816d6473be497ad796e98
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 <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>
2023-10-23 21:12:15 +0300

    return update

--- api/routerHelper.js
+++ api/routerHelper.js
@@ -1,6 +1,6 @@
 export default function RouterHelper(server,router){
     server.post('/landlord/login', (req, res) => {
  
-        res.status(201).json(router.db.get('posts'));
+        res.status(201).json(router.db.get('authLogin'));
       });
 }

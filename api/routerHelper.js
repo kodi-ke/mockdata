6abbca9b1f626944a3d342324e2186c351f5a10d
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 <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>
2023-10-23 21:24:39 +0300

    auth routes

--- api/routerHelper.js
+++ api/routerHelper.js
@@ -3,4 +3,16 @@ export default function RouterHelper(server,router){
  
         res.status(201).json(router.db.get('authLogin'));
       });
+      server.post('/tenant/login', (req, res) => {
+ 
+        res.status(201).json(router.db.get('tenant'));
+      });
+      server.post('/caretaker/login', (req, res) => {
+ 
+        res.status(201).json(router.db.get('caretaker'));
+      });
+      server.post('/agent/login', (req, res) => {
+ 
+        res.status(201).json(router.db.get('agent'));
+      });
 }
